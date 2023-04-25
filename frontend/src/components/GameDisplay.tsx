b4de15b9fbfd54a119195f4cb3436b763a6a5b29
hhan0014 <hhan0014@student.monash.edu>
2023-04-25 14:10:31 +0000

    Player can select, deselect and move the piece. Player can move the piece to its adjacent position.

--- frontend/src/components/GameDisplay.tsx
+++ frontend/src/components/GameDisplay.tsx
@@ -5,41 +5,66 @@ import PiecesLeft from "./PiecesLeft";
 import Piece from "./Piece";
 
 const GameDisplay = () => {
-    // Initialize the game
-    const [game, setGame] = useState(new Game());
+  // Initialize the game
+  const [game, setGame] = useState(new Game());
 
-    const handlePieceClick = (index: number) => {
-        // Place the piece
-        game.getBoard().setPiece(index, game.getCurrentPlayer());
-        game.getCurrentPlayer().decrementPiecesLeft();
-        game.setCurrentPlayer(game.getCurrentPlayer().getColour() === "white" ? game.getPlayerBlack() : game.getPlayerWhite());
+  const handlePieceClick = (index: number) => {
+    // Place the piece
+    if (game.getCurrentPlayer().getPiecesLeft() > 0) {
+      game.getBoard().setPiece(index, game.getCurrentPlayer());
+      game.getCurrentPlayer().decrementPiecesLeft();
+      game.setCurrentPlayer(
+        game.getCurrentPlayer().getColour() === "white"
+          ? game.getPlayerBlack()
+          : game.getPlayerWhite()
+      );
+    } else {
+      //Select Piece
+      if (game.getBoard().getSelectedPiece() == -1) {
+        game.getBoard().checkSelectedPiece(index, game.getCurrentPlayer());
+      }
+      // Move Piece
+      else {
+        game.getBoard().movePiece(index, game.getCurrentPlayer());
+        if (game.getBoard().getIsPieceMoved()) {
+          game.getBoard().setIsPieceMoved(false);
+          game.setCurrentPlayer(
+            game.getCurrentPlayer().getColour() === "white"
+              ? game.getPlayerBlack()
+              : game.getPlayerWhite()
+          );
+        }
+      }
+    }
 
-        // Update the game state
-        const gameState = game.getState();
-        setGame(new Game(gameState));
-    };
+    // Update the game state
+    const gameState = game.getState();
+    setGame(new Game(gameState));
+  };
 
-    // Render the game board
-    return (
-        <div className="flex flex-col items-center justify-center">
-            <div className="mb-10 flex justify-center gap-2 bg-amber-100 p-4 rounded text-black w-60">
-                <Piece colour={game.getCurrentPlayer().getColour()} />
-                <h3 className=" text-lg">{game.getCurrentPlayer().getColour()}'s turn to play</h3>
-            </div>
-            <section className="flex justify-between gap-12">
-                <div className="grid items-center">
-                    <PiecesLeft player={game.getPlayerWhite()} />
-                </div>
-                <Board game={game} handlePieceClick={handlePieceClick} />
-                <div className="grid items-center">
-                    <PiecesLeft player={game.getPlayerBlack()} />
-                </div>
-            </section>
-            <button className="mt-8" onClick={() => setGame(new Game())}>
-                Reset
-            </button>
+  // Render the game board
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="mb-10 flex justify-center gap-2 bg-amber-100 p-4 rounded text-black w-60">
+        <Piece colour={game.getCurrentPlayer().getColour()} />
+        <h3 className=" text-lg">
+          {game.getCurrentPlayer().getColour()}'s turn to play
+        </h3>
+      </div>
+      <section className="flex justify-between gap-12">
+        <div className="grid items-center">
+          <PiecesLeft player={game.getPlayerWhite()} />
         </div>
-    );
+        <Board game={game} handlePieceClick={handlePieceClick} />
+        <div className="grid items-center">
+          <PiecesLeft player={game.getPlayerBlack()} />
+        </div>
+      </section>
+      <button className="mt-8" onClick={() => setGame(new Game())}>
+        Reset
+      </button>
+    </div>
+  );
 };
 
 export default GameDisplay;
--- frontend/src/models/Board.ts
+++ frontend/src/models/Board.ts
@@ -6,13 +6,15 @@ import { Player } from './Player'
 export class Board {
     private positions: (Position | null)[]
     private mills: { [key: string]: Mill }
-    private selectedPiece?: Piece
+    private selectedPiece: number
+    private isPieceMoved: boolean
+    private validMove: number[]
 
     constructor() {
         this.positions = Array(49).fill(null)
 
         const validPositions = [0, 3, 6, 8, 10, 12, 16, 17, 18, 21, 22, 23, 25, 26, 27, 30, 31, 32, 36, 38, 40, 42, 45, 48]
-        const neighbours = [[3,21], [0,6,10], [3,27], [10,22], [3,8,12,17], [10,26], [17.23], [10,16,18], [17,25], [0,22,42], [8,21,23,36], [16,22,30], [18,26,32], [12,25,27,40], [6,26,48], [23,31], [30,32,38], [25,31], [22,38], [31,36,40,45], [26,38], [21,45], [38,42,48], [27,45]]
+        const neighbours = [[3, 21], [0, 6, 10], [3, 27], [10, 22], [3, 8, 12, 17], [10, 26], [17, 23], [10, 16, 18], [17, 25], [0, 22, 42], [8, 21, 23, 36], [16, 22, 30], [18, 26, 32], [12, 25, 27, 40], [6, 26, 48], [23, 31], [30, 32, 38], [25, 31], [22, 38], [31, 36, 40, 45], [26, 38], [21, 45], [38, 42, 48], [27, 45]]
 
         for (let i = 0; i < validPositions.length; i++) {
             const index = validPositions[i]
@@ -38,6 +40,9 @@ export class Board {
             '25-27': new Mill([this.getPosition(25), this.getPosition(26), this.getPosition(27)]),
             '31-45': new Mill([this.getPosition(31), this.getPosition(38), this.getPosition(45)]),
         }
+        this.isPieceMoved = false
+        this.selectedPiece = -1
+        this.validMove = []
     }
 
     // Helper method to get positions
@@ -69,8 +74,6 @@ export class Board {
         const position = this.getPosition(index)
         if (position.getPiece()) {
             throw new Error(`There is already a piece at (${index})`)
-        } else if (currentPlayer.getPiecesLeft() === 0) {
-            throw new Error(`Player ${currentPlayer.getColour()} has no pieces left`)
         } else {
             position.setPiece(new Piece(currentPlayer.getColour()))
         }
@@ -86,19 +89,96 @@ export class Board {
         }
     }
 
-    // Get selected piece
-    getSelectedPiece(): Piece | undefined {
-        return this.selectedPiece
+    movePiece(index: number, currentPlayer: Player) {
+        const position = this.getPosition(index)
+        if (this.selectedPiece === index) {
+            this.resetCheckMove()
+            console.log("Deselect piece")
+        }
+        else if (!this.validMove.includes(index)) {
+            console.log("Please choose the valid move only.")
+        }
+        else {
+            if (!position.getPiece()) {
+                //Remove the piece from old position
+                this.getPosition(this.getSelectedPiece()).setPiece(undefined)
+                //Place the piece in new position
+                position.setPiece(new Piece(currentPlayer.getColour()))
+                this.isPieceMoved = true
+                this.resetCheckMove()
+                console.log("Place piece here")
+            }
+        }
+    }
+
+    calculateValidMove(index: number, currentPlayer: Player): boolean {
+        const position = this.getPosition(index)
+        if (position.getPiece()) {
+            if (currentPlayer.getPiecesLeft() == 0 && currentPlayer.getColour() == position.getPiece()?.getColour()) {
+                console.log("Calculate the move")
+                for (const index of position.getNeighbours()) {
+                    if (index !== undefined && this.getPosition(index).getPiece() === undefined) {
+                        this.validMove.push(index)
+                    }
+                }
+                console.log("Finish calculate the move")
+                if (this.validMove.length === 0) {
+                    // If the loop did not find a valid move, throw an error
+                    console.log("No valid move for this piece.")
+                }
+                else {
+                    console.log("There is valid move(s)")
+                    return true;
+                }
+            }
+            else {
+                console.log(`There is already a piece at (${index})`)
+            }
+        }
+        return false;
     }
 
     // Select a piece on the board
-    selectPiece(piece: Piece) {
-        this.selectedPiece = piece
+    checkSelectedPiece(index: number, currentPlayer: Player) {
+        const position = this.getPosition(index)
+        if (position.getPiece()) {
+            if (currentPlayer.getColour() == position.getPiece()?.getColour()) {
+                if (this.calculateValidMove(index, currentPlayer) == true) {
+                    this.setSelectedPiece(index)
+                    console.log("Select Piece")
+                }
+                else {
+                    console.log("This piece can't be moved. Please choose different piece!")
+                }
+            }
+            else {
+                console.log("Please choose your color piece")
+            }
+        }
+        else {
+            console.log(`There is no piece at (${index})`)
+        }
     }
 
-    // Deselect the currently selected piece
-    deselectPiece() {
-        this.selectedPiece = undefined
+    setSelectedPiece(index: number) {
+        this.selectedPiece = index
+    }
+
+    // Get selected piece
+    getSelectedPiece(): number {
+        return this.selectedPiece
+    }
+
+    getIsPieceMoved(): boolean {
+        return this.isPieceMoved
+    }
+    setIsPieceMoved(isMoved: boolean) {
+        this.isPieceMoved = isMoved
+    }
+
+    resetCheckMove() {
+        this.selectedPiece = -1
+        this.validMove = []
     }
 
 }

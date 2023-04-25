3d5d0c768134fd71a6ff60c0834b2812216a56ef
Liangdi <lwan0141@student.monash.edu>
2023-04-25 13:37:02 +1000

    increase rounding on pieces

--- frontend/src/components/PiecesLeft.tsx
+++ frontend/src/components/PiecesLeft.tsx
@@ -10,7 +10,7 @@ const PiecesLeft = ({ player }: Props) => {
         <div className="w-16 h-44 p-2 rounded bg-amber-600 flex flex-col gap-0.5 ring-[0.4rem] ring-inset ring-black/20">
             {/* <div className={`w-full h-4 rounded ${pieceColour}`}></div> */}
             {Array.from({ length: player.getPiecesLeft() }).map((item, index) => (
-                <div key={index} className={`w-full h-4 rounded ${player.getColour() === "white" ? "bg-white" : "bg-black"}`}></div>
+                <div key={index} className={`w-full h-4 rounded-lg ${player.getColour() === "white" ? "bg-white" : "bg-black"}`}></div>
             ))}
         </div>
     )

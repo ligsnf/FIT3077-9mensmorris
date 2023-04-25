a641337eaa8306e135a0bd5979d35ce7e048d854
Liangdi <lwan0141@student.monash.edu>
2023-04-25 11:53:56 +1000

    add border to pieces left ui

--- frontend/src/components/PiecesLeft.tsx
+++ frontend/src/components/PiecesLeft.tsx
@@ -21,7 +21,7 @@ const PiecesLeft = ({ player }: Props) => {
     }
 
     return (
-        <div className="w-16 h-44 p-2 rounded bg-amber-600 flex flex-col gap-0.5">
+        <div className="w-16 h-44 p-2 rounded bg-amber-600 flex flex-col gap-0.5 ring-[0.4rem] ring-inset ring-black/20">
             {/* <div className={`w-full h-4 rounded ${pieceColour}`}></div> */}
             {Array.from({ length: player.getPiecesLeft() }).map((item, index) => (
                 <div key={index} className={`w-full h-4 rounded ${pieceColour}`}></div>

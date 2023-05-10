21ba9fa25dcdb13672dee764338fd6579544f92f
Liangdi <lwan0141@student.monash.edu>
2023-05-10 12:02:15 +1000

    add logic to player to support basic rules

--- frontend/src/models/Player.ts
+++ frontend/src/models/Player.ts
@@ -3,10 +3,23 @@ import { PieceColour } from './Piece';
 export class Player {
     private colour: PieceColour;
     private piecesLeft: number;
+    private piecesTaken: number;
+    private piecesOnBoard: number;
 
     constructor(colour: PieceColour) {
         this.colour = colour;
         this.piecesLeft = 9;
+        this.piecesTaken = 0;
+        this.piecesOnBoard = 0;
+    }
+
+    getMoveType(): string {
+        if (this.piecesLeft > 0)
+            return "Place";
+        else if (this.piecesOnBoard <= 3)
+            return "Fly";
+        else
+            return "Move";
     }
 
     getColour(): PieceColour {
@@ -20,6 +33,26 @@ export class Player {
     decrementPiecesLeft(): void {
         if (this.piecesLeft - 1 !== -1)
             this.piecesLeft--;
+    }
+
+    getPiecesTaken(): number {
+        return this.piecesTaken;
+    }
+
+    incrementPiecesTaken(): void {
+        this.piecesTaken++;
+    }
+
+    getPiecesOnBoard(): number {
+        return this.piecesOnBoard;
+    }
+
+    incrementPiecesOnBoard(): void {
+        this.piecesOnBoard++;
+    }
+
+    decrementPiecesOnBoard(): void {
+        this.piecesOnBoard--;
     }
 
 }
